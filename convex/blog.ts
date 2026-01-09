c23abfd98c4e242840ad4bdaca0b8c05119a8ba7
shinehtetnaing <shinehtetnaing1998@gmail.com>
2026-01-09 14:19:41 +0700

    blog create convex function

---
+++ convex/blog.ts
@@ -0,0 +1,22 @@
+import { ConvexError, v } from "convex/values";
+import { mutation } from "./_generated/server";
+import { authComponent } from "./auth";
+
+// Create a new blog with the given title and content
+export const createBlog = mutation({
+  args: { title: v.string(), content: v.string() },
+  handler: async (ctx, args) => {
+    const user = await authComponent.safeGetAuthUser(ctx);
+
+    if (!user) {
+      throw new ConvexError("Not authenticated");
+    }
+
+    const newBlogId = await ctx.db.insert("blogs", {
+      title: args.title,
+      content: args.content,
+      authorId: user._id,
+    });
+    return newBlogId;
+  },
+});
